aac70eb70d0a04fe863eae86973e6618081902de
bqeanzol <bqeanzol@us.ibm.com>
2017-12-11 05:35:02 -0400

    shoot it app

---
+++ shooting-app/position_test.py
@@ -0,0 +1,42 @@
+
+import unittest
+
+
+from shooting import Position
+
+class PositionTest(unittest.TestCase):
+
+	def test_position_can_be_created_with_no_params(self):
+		position = Position()
+		self.assertTrue(position != None)
+
+	def test_position_with_no_params_corresponds_to_origin(self):
+		position = Position()
+		self.assertEqual(0, position.x())
+		self.assertEqual(0, position.y())
+
+	def test_position_can_be_created_with_x_and_y_params(self):
+		position = Position(2, 3)
+		self.assertEqual(2, position.x())
+		self.assertEqual(3, position.y())
+
+	def test_distance_to_same_position_is_zero(self):
+		position = Position(1, 3)
+		self.assertEqual(0, position.distance_to(position))
+
+	def test_distance_is_greater_than_zero_if_positions_are_different(self):
+		first_pos = Position()
+		second_pos = Position(1, 1)
+
+		self.assertTrue(first_pos.distance_to(second_pos) > 0)
+
+	def test_positions_with_same_coordinates_are_equal(self):
+		first_pos = Position(1, 2)
+		second_pos = Position(1, 2)
+
+		self.assertTrue(first_pos == second_pos)
+		self.assertEqual(first_pos, second_pos)
+
+
+if __name__ == "__main__":
+	unittest.main()
---
+++ shooting-app/shooting.py
@@ -0,0 +1,50 @@
+import math
+
+class Position(object):
+
+	def __init__(self, x=0, y=0):
+		self._x = x
+		self._y = y
+
+	def x(self):
+		return self._x
+
+	def y(self):
+		return self._y
+
+	def distance_to(self, other):
+		delta_x = self.x() - other.x()
+		delta_y = self.y() - other.y()
+		return math.sqrt(delta_x ** 2 + delta_y ** 2)
+
+	def __eq__(self, other):
+		return True	
+
+class Velocity(object):
+
+	def __init__(self, start, end):
+		self.start = start
+		self.end = end
+
+	def magnitude(self):
+		return self.end.distance_to(self.start)
+
+	def angle(self):
+		# angle = tan-1(y/x)
+		delta_y = self.end.y() - self.start.y()
+		delta_x = self.end.x() - self.start.x()
+		return math.degrees(math.atan2(delta_y, delta_x))
+
+
+class Projectile(object):
+	
+	def __init__(self, position):
+		self._position = position;
+
+	def shoot(self, velocity):
+		pass
+
+	def position(self):
+		return self._position
+
+
