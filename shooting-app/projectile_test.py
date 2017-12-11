df0d8921b96313aa1fe21ae3951b0d1112c56dd6
bqeanzol <bqeanzol@us.ibm.com>
2017-12-11 14:37:02 -0400

    test previous version

--- shooting-app/projectile_test.py
+++ shooting-app/projectile_test.py
@@ -10,28 +10,34 @@ class ProjectileTest(unittest.TestCase):
 
 		self.assertTrue(isinstance(projectile, Projectile))
 
-	def test_position_changes_when_projectile_is_shoot(self):
-
-		initial_position = Position()
-		projectile = Projectile(initial_position)
-		velocity = Velocity(Position(), Position(3, 5))
-
-		projectile.shoot(velocity)
-
-		self.assertTrue(isinstance(projectile.position(), Position))
-		self.assertNotEquals(initial_position, projectile.position())
-
-	def test_projectile_shot_to_the_floor_should_remain_there(self):
-		initial_position = Position(5, 10)
-		projectile = Projectile(initial_position)
-		velocity = Velocity(Position(), Position(0, -1))
-
-		projectile.shoot(velocity)
-
-		self.assertEquals(Position(5, 0), projectile.position())
+	# def test_position_changes_when_projectile_is_shoot(self):
+    #
+	# 	initial_position = Position()
+	# 	projectile = Projectile(initial_position)
+	# 	velocity = Velocity(Position(), Position(3, 5))
+    #
+	# 	projectile.shoot(velocity)
+    #
+	# 	self.assertTrue(isinstance(projectile.position(), Position))
+	# 	self.assertNotEqual(initial_position, projectile.position())
+    #
+	# def test_projectile_shot_to_the_floor_should_remain_there(self):
+	# 	initial_position = Position(5, 10)
+	# 	projectile = Projectile(initial_position)
+	# 	velocity = Velocity(Position(), Position(0, -1))
+    #
+	# 	projectile.shoot(velocity)
+    #
+	# 	self.assertEqual(Position(5, 0), projectile.position())
 
 	def test_projectile_shot_with_a_given_angle_follows_a_parabolic_path(self):
-		pass
+		initial_position = Position(0, 3)
+		projectile = Projectile(initial_position)
+		velocity = Velocity(Position(0,3), Position(4,6))
+		#angle = velocity.angle()
+		projectile.shoot(velocity)
+		print(projectile.time)
+		self.assertEqual(Position(15,0), projectile.position())
 
 if __name__ == "__main__":
 	unittest.main()
--- shooting-app/shooting.py
+++ shooting-app/shooting.py
@@ -1,5 +1,21 @@
 import math
 
+def cuadratic_equation (a, b, c):
+	x = []
+	d = b ** 2 - 4 * a * c  # discriminant
+	print ("d: ",d)
+	if d < 0:
+		return [] #"This equation has no real solution"
+	elif d == 0:
+		x = [-b / 2 * a]
+		return x #	"This equation has one solutions: ", x
+	else: #"This equation has two solutions: ", x1, " and", x2
+		x1 = (-b - math.sqrt(d)) / (2 * a)
+		x2 = (-b + math.sqrt(d)) / (2 * a)
+		x = [x1, x2]  # returns first the positive solution for a positive time
+		return (x)
+
+
 class Position(object):
 
 	def __init__(self, x=0, y=0):
@@ -19,7 +35,7 @@ class Position(object):
 
 	def __eq__(self, other):
 		if (isinstance(other, Position)):
-			return self.x() == other.x() and self.y() == other.y()
+			return (self.x() == other.x() and self.y() == other.y())
 		else:
 			return False
 
@@ -32,25 +48,54 @@ class Velocity(object):
 		self.start = start
 		self.end = end
 
+	def delta_x (self):
+		return self.end.x() - self.start.x()
+
+	def delta_y (self):
+		return self.end.y() - self.start.y()
+
 	def magnitude(self):
 		return self.end.distance_to(self.start)
 
 	def angle(self):
 		# angle = tan-1(y/x)
-		delta_y = self.end.y() - self.start.y()
-		delta_x = self.end.x() - self.start.x()
-		return math.degrees(math.atan2(delta_y, delta_x))
+		return math.degrees(math.atan2(self.delta_y(), self.delta_x()))
 
 
 class Projectile(object):
 	
 	def __init__(self, position):
 		self._position = position;
+		self.time = None
+	def position (self):
+		return (self.positon)
+
 
 	def shoot(self, velocity):
-		pass
+		# Changes the inital postion to a new postion
+		angle = velocity.angle()
+		# The final position will be 0 for y then
+		a = -1/2*9.8 # from  -1/2*g*t**2
+		print ("a:", a)
+		b = velocity.delta_y()
+		print("b:", b)
+		c = velocity.start.y()
+		print("c:", c)
+		t = cuadratic_equation(a, b, c)
+		print (t)
+		if t[0] >= 0:
+			self.time = t[0]
+		else:
+			self.time = t[1]
 
-	def position(self):
-		return Position(5, 0)
+		# Calculating the final position
+		x = velocity.start.x() + velocity.delta_x()*self.time
+		position = Position(x,0)
+		self.position = position
+
+
+
+
+
 
 
